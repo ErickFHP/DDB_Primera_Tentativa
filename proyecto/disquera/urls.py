d9ff4d04fdc1f53defe747dd004ff57ec1ad1b22
Erick Hernández <84101451+ErickFHP@users.noreply.github.com>
2024-04-27 21:13:57 -0600

    Delete proyecto directory

--- proyecto/disquera/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('artistas', views.artistas, name='artistas'),
-    path('catalogo', views.catalogo, name='catalogo'),
-]
